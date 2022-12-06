29f66a38cfd8626f8fdac89be45d160305973695
Rob Kayson <rkayson@gmail.com>
2022-12-06 12:45:26 -0800

    Pass points to Line draw function

--- index.js
+++ index.js
@@ -5,21 +5,26 @@ let ln
 
 function setup() {
   createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT)
-  ln = new Line()
+  ln = new Line(4)
 }
 
 function draw() {
   background(100)
-  ln.draw()
+  ln.draw([createVector(width / 2, height / 2), createVector(mouseX, mouseY)])
 }
 
-function Line() {
-  this.points = [createVector(10, 10), createVector(CANVAS_WIDTH - 10, 10)]
+function Line(strokeWidth) {
+  this.strokeWidth = strokeWidth
 
-  this.draw = () => {
-    for (let i = 0; i < this.points.length - 1; i++) {
-      const from = this.points[i]
-      const to = this.points[i + 1]
+  this.draw = points => {
+    if (points.length < 2) {
+      return
+    }
+
+    strokeWeight(this.strokeWidth)
+    for (let i = 0; i < points.length - 1; i++) {
+      const from = points[i]
+      const to = points[i + 1]
       line(from.x, from.y, to.x, to.y)
     }
   }
