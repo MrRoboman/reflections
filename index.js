7e547d74509d2f47181d223c7f59f67084e5fba9
Rob Kayson <rkayson@gmail.com>
2022-12-06 16:00:27 -0800

    Add reflections in multiple mirrors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const CANVAS_WIDTH = 600
-const CANVAS_HEIGHT = 600
+const CANVAS_WIDTH = 800
+const CANVAS_HEIGHT = 800
 
-let mirror
+let mirrors
 
 function setup() {
   createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT)
-  mirror = new Line(width * 0.6, height * 0.25, width * 0.6, height * 0.75)
+  mirrors = [
+    new Line(width * 0.4, height * 0.25, width * 0.4, height * 0.75),
+    new Line(width * 0.6, height * 0.25, width * 0.6, height * 0.75),
+  ]
 
   strokeWeight(4)
 }
@@ -15,10 +18,10 @@ function draw() {
 
   const ln = new Line(width * 0.5, height * 0.5, mouseX, mouseY)
 
-  const reflectionLines = getReflectionLines(ln, mirror)
+  const reflectionLines = getReflectionLines(ln, mirrors)
 
   reflectionLines.forEach(ln => ln.draw())
-  mirror.draw()
+  mirrors.forEach(m => m.draw())
 }
 
 function Line(startX, startY, endX, endY) {
@@ -38,27 +41,46 @@ function Line(startX, startY, endX, endY) {
   }
 }
 
-function getReflectionLines(sightLine, mirror) {
-  const lines = [sightLine]
+function getReflectionLines(sightLine, mirrors, ignoreMirror) {
+  for (let i = 0; i < mirrors.length; i++) {
+    const mirror = mirrors[i]
 
-  const intersection = findIntersection(sightLine, mirror)
+    if (mirror === ignoreMirror) {
+      continue
+    }
 
-  if (!intersection) {
-    return lines
+    const intersection = findIntersection(sightLine, mirror)
+
+    if (intersection) {
+      const firstHalfOfLine = new Line(
+        sightLine.start.x,
+        sightLine.start.y,
+        intersection.x,
+        intersection.y,
+      )
+
+      // TODO: This assumes the mirror is vertical, normal should be based on the angle of the mirror.
+      const reflectionNormal = createVector(1, 0)
+
+      const reflection = p5.Vector.sub(sightLine.end, intersection).reflect(
+        reflectionNormal,
+      )
+
+      const end = p5.Vector.add(intersection, reflection)
+      const reflectedHalfOfLine = new Line(
+        intersection.x,
+        intersection.y,
+        end.x,
+        end.y,
+      )
+
+      return [firstHalfOfLine].concat(
+        getReflectionLines(reflectedHalfOfLine, mirrors, mirror),
+      )
+    }
   }
 
-  // Temporary line to get the normal, this should be based on the mirror
-  const reflectionNormal = createVector(1, 0)
-  const reflection = p5.Vector.sub(sightLine.end, intersection).reflect(
-    reflectionNormal,
-  )
-  const start = intersection
-  const end = p5.Vector.add(intersection, reflection)
-  const ln = new Line(start.x, start.y, end.x, end.y)
-
-  lines.push(ln)
-
-  return lines
+  return [sightLine]
 }
 
 // Adapted from
@@ -78,6 +100,7 @@ function findIntersection(lineA, lineB) {
     const ub = nb / denom
 
     if (ua >= 0.0 && ua <= 1.0 && ub >= 0.0 && ub <= 1.0) {
+      console.log('fart')
       return lineA.interpolate(ua)
     }
   }
